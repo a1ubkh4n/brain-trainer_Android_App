c09f394df3fe2b9a44eb8fce774edf953db3879c
a1ubkh4n <aiubkhan95@gmail.com>
2018-07-18 22:59:44 +0600

    hidder button

--- app/src/main/java/khanshaheb/a1ubkh4n/com/brain_trainer_app/MainActivity.java
+++ app/src/main/java/khanshaheb/a1ubkh4n/com/brain_trainer_app/MainActivity.java
@@ -2,12 +2,26 @@ package khanshaheb.a1ubkh4n.com.brain_trainer_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = (Button) findViewById(R.id.startButton);
+
+    }
+
+
+    public void start(View view) {
+
+        startButton.setVisibility(View.INVISIBLE);
+
     }
 }
